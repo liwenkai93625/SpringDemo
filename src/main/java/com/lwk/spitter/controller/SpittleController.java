2ba53a512449913c1d8ce8d72c277c424148f003
李文凯 <liwenkai@lianjia.com>
2017-05-19 19:06:44 +0800

    更新SpittleController.java

--- src/main/java/com/lwk/spitter/controller/SpittleController.java
+++ src/main/java/com/lwk/spitter/controller/SpittleController.java
@@ -1,11 +1,16 @@
 package com.lwk.spitter.controller;
 
 import com.lwk.spitter.data.SpittleRepository;
+import com.lwk.spitter.model.Spittle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
  * Created by lianjia on 2017-5-19.
@@ -14,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/spittles")
 public class SpittleController {
 
+    private static final String MAX_LONG_AS_STRING =
+            Long.toString(Long.MAX_VALUE);
     private SpittleRepository spittleRepository;
 
     @Autowired
@@ -27,5 +34,14 @@ public class SpittleController {
                 spittleRepository.findSpittles(Long.MAX_VALUE, 20)
         );
         return "spittles";
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Spittle> spittles(
+            @RequestParam(value = "max",
+                    defaultValue = MAX_LONG_AS_STRING) long max,
+            @RequestParam(value = "count",
+                    defaultValue = "20") int count) {
+        return spittleRepository.findSpittles(max, count);
     }
 }
